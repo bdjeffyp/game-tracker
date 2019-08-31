17b0ccdae4fad912e87c0a5bb4b18c91e0c604d6
bdjeffyp <bdjeffyp@koji-projects.com>
2019-08-31 04:19:23 +0000

    Manual deploy

--- frontend/index.tsx
+++ frontend/index.tsx
@@ -1,11 +1,8 @@
-import * as Koji from "koji-tools";
+import * as Koji from "@withkoji/vcc";
 import * as React from "react";
 import * as ReactDom from "react-dom";
 
 import App from "./src/App";
-
-// Used for Koji integration. Do not remove unless you don't want to use with Koji.
-Koji.pageLoad();
 
 ReactDom.render(
   <App />,
