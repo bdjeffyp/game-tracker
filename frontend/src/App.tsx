d3799cbe8331e572f5ddd7c5fe192fc0bcbc549c
Jeff <bdjeffyp@users.noreply.github.com>
2019-09-09 20:08:44 -0700

    Package updates

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,10 @@ import * as React from "react";
 
 export default class App extends React.Component {
   public render(): JSX.Element {
-      return <button onClick={this._buttonClicked}>Hey there!</button>;
+    return <button onClick={this._buttonClicked}>Hey there!</button>;
   }
 
   private _buttonClicked = (): void => {
-      alert("Hello, world!");
+    alert("Hello, world!");
   }
 }
